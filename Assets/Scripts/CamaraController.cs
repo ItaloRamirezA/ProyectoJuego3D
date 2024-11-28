a83d730d463a01f004695d5cf46f8fb5a18c8284
ItaloRamirezA <113093861+ItaloRamirezA@users.noreply.github.com>
2024-11-28 19:07:07 +0100

    camara gira

--- Assets/Scripts/CamaraController.cs
+++ Assets/Scripts/CamaraController.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class CamaraController : MonoBehaviour
 {
+    private Transform camaraTransform;
     public Vector2 sensibilidad;
 
     void Start()
     {
+        camaraTransform = transform.Find("--- MAIN CAMERA ---");
+        
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         float horizontal = Input.GetAxis("Mouse X");
+        float vertical = Input.GetAxis("Mouse Y");
 
         if (horizontal != 0) {
             transform.Rotate(Vector3.up * horizontal * sensibilidad.x);
+        }
+
+        if (vertical != 0) {
+            camaraTransform.Rotate(Vector3.left * vertical * sensibilidad.y);
         }
     }
 }
