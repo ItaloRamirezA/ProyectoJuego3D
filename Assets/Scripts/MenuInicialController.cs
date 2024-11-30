600a5409d75fce68d6ea61828bea763ecb504720
ItaloRamirezA <113093861+ItaloRamirezA@users.noreply.github.com>
2024-11-30 16:20:36 +0100

    menu inicio

---
+++ Assets/Scripts/MenuInicialController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicialController : MonoBehaviour
+{
+    public void jugar() {
+        SceneManager.LoadScene("Nivel1");
+    }
+
+    public void controles() {
+        
+    }
+
+    public void salir() {
+        Application.Quit();
+    }
+}
